5547a553bff1834b7468fdc1b80c71b5e2635a8a
Furkan <caliskanfurkan@gmail.com>
2015-10-11 20:30:38 +0000

    dua

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '£#$#£$#£$#£$343Fa
-wd##24'
+app.config['SECRET_KEY'] = '£#$#£$#£$#£$343Fa'
+db = SQLAlchemy(app)
+
+class Zaafiyet(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	aciklama = db.Column(db.String(100), unique=False)
+	
+	def __init__(self, aciklama):
+		self.aciklama = aciklama
+	
+	def __repr__(self):
+		return '<Zaafiyet %r>' % self.aciklama
 
 @app.route("/")
 def main():
@@ -11,16 +23,25 @@ def main():
 
 def check_login(username, password):
 	if username == "furkan" and password == "123456":
-		session['user'] = "furkan"
 		return True
 	else:
 		return False
+
+@app.route("/site")
+def iceri():
+	try:
+		if session['logged_in'] == True:
+	 		return session['username']
+	except KeyError:
+		return "Defol"
 
 @app.route("/login", methods=['POST','GET'])
 def login():
         if request.method == 'POST':
 		if check_login(request.form['username'], request.form['pass']):
-			return redirect(url_for('main')) 
+			session['logged_in'] = True
+			session['username'] = request.form['username'] 
+			return redirect(url_for('iceri')) 
 		else:
 			return "O iş olmaz"
 			
